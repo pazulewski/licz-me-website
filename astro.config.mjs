1f2828c2b6cd149ee70f6d3a2ea273488e3c2e44
pazulewski <83099929+pazulewski@users.noreply.github.com>
2025-08-07 11:23:19 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,4 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({});
 
 
-<!-- ====================================================================== -->
-<!-- FILE: tsconfig.json (KLUCZOWY PLIK) -->
-<!-- Umieść ten plik w głównym folderze projektu. -->
-<!-- ====================================================================== -->
-
-{
-  "extends": "astro/tsconfigs/base"
-}
 
