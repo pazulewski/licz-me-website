8892002c11316ff3b364a180dc82f5e83ad0d49c
pazulewski <83099929+pazulewski@users.noreply.github.com>
2025-08-12 14:01:09 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
-
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-  // WAŻNE: Podaj tutaj adres swojej strony
   site: 'https://www.licz.me',
   integrations: [tailwind(), sitemap()]
 });
-
