ff8da201569b77f435798fe5e98481f85d371459
pazulewski <83099929+pazulewski@users.noreply.github.com>
2025-09-15 11:26:43 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+// Ta wersja nie zawiera już błędnego importu sitemap.
 export default defineConfig({
   site: 'https://www.licz.me',
   trailingSlash: 'never',
